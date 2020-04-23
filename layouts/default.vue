e087736823369309bc8d56fc2d41f3750a7eefcb
Jimmy Roland <jimmylroland@gmail.com>
2020-04-23 11:27:30 -0500

    remove ad placements till ads are active

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,11 @@
   <div :style="{ background: '#f2f2f2' }">
     <DealHeader />
     <el-row>
-      <el-col :lg="3" :sm="24" :xs="24"><adsbygoogle /></el-col>
+      <el-col :lg="3" :sm="24" :xs="24">AD AD AD</el-col>
       <el-col :lg="18" :sm="24" :xs="24">
         <nuxt />
       </el-col>
-      <el-col :lg="3" :sm="24" :xs="24"><adsbygoogle /></el-col>
+      <el-col :lg="3" :sm="24" :xs="24">AD AD AD</el-col>
     </el-row>
   </div>
 </template>
